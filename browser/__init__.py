83e3977c29396ad7d0fab3597aac5d714e4e2085
zacatac <field.zackery@gmail.com>
2014-07-28 15:13:08 -0400

    Functional windows version. Runs locally with "python remote.py". requirements.txt up-to-date

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,19 +1,26 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, ElementNotVisibleException, UnexpectedAlertPresentException
 
-# chromedriver_path = '/Users/zrfield/laserstrike/chromedriver'
-driver = webdriver.PhantomJS() 
-driver.get("http://192.168.10.103/lasertag/lt_game/game_main.php")
-
+#executeable_path = 'C:\Program Files (x86)\phantomjs-1.9.7-windows\phantomjs'
+chromedriver_path = 'C:\Users\LasertagDesk\chromedriver'
+os.environ["webdriver.chrome.driver"] = chromedriver_path
+driver = webdriver.Chrome(chromedriver_path) 
 wait = WebDriverWait(driver, 10) 
+
+driver.get("http://192.168.10.103/lasertag/login.php?ref=/lasertag/lt_game/game_main.php")
+
 
 assert "LOGIN" in driver.title
 elem = driver.find_element_by_name("password")
 elem.send_keys("play")
 elem.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(5)
 
 ready_button = driver.find_element_by_id("readybutton")
 start_button = driver.find_element_by_id("startbutton")
@@ -21,4 +28,3 @@ save_button = driver.find_element_by_id("savebutton")
 print_button = driver.find_element_by_id("printbutton")
 receive_button = driver.find_element_by_id("receivebutton")
 abort_button = driver.find_element_by_id("abortbutton")
-
--- remote.py
+++ remote.py
@@ -1,57 +1,60 @@
 from flask import Flask, render_template, url_for, redirect
 from browser import driver, Keys, ready_button, start_button, save_button, print_button, receive_button, abort_button, wait, EC, By
+from browser import StaleElementReferenceException, ElementNotVisibleException, UnexpectedAlertPresentException
 app = Flask(__name__)
 
 buttons = {
-    'ready':ready_button.is_displayed,
-    'start':start_button.is_displayed,
-    'printb':print_button.is_displayed,
-    'receive':ready_button.is_displayed,
-    'save':save_button.is_displayed,
-    'abort':abort_button.is_displayed
+    'ready':ready_button,
+    'start':start_button,
+    'print':print_button,
+    'receive':receive_button,
+    'save':save_button,
+    'abort':abort_button
 }
 
-local_buttons = {    
-    'ready':ready_button.is_displayed,
-    'start':start_button.is_displayed,
-    'printb':print_button.is_displayed,
-    'receive':ready_button.is_displayed,
-    'save':save_button.is_displayed,
-    'abort':abort_button.is_displayed
-}
+name = "none"
 
 def visible_buttons(buttons):
     visible = {}
     for key in buttons.keys():
-        visible[key] = buttons[key]()
+        try:
+            visible[key] = buttons[key].is_displayed()
+        except StaleElementReferenceException:
+            visible[key] = False
+            try:
+                buttons[key] = driver.find_element_by_id("%sbutton" % key)
+            except ElementNotVisibleException:
+                pass
     return visible
     
 @app.route('/')
-def index():    
-    return render_template('index.html', **visible_buttons(buttons))
+def index():   
+    global name 
+    time = driver.find_element_by_id("gametime").text
+    num = driver.find_element_by_id("gamenumber").text
+    return render_template('index.html', time=time, num=num, name=name, **visible_buttons(buttons))
 
 @app.route('/ready')
 def ready():    
-    ready_button.click()
-    wait.until(EC.visibility_of(start_button))
+    buttons["ready"].click()
+    wait.until(EC.visibility_of(buttons["start"]))
     return redirect(url_for('index'))
 
 @app.route('/start')
 def start():    
-    start_button.click()
+    buttons["start"].click()
     wait.until(EC.invisibility_of_element_located((By.ID, 'startbutton')))
     return redirect(url_for('index'))
 
 @app.route('/save')
 def save():    
-    print('saving')
-    save_button.click()
-    wait.until(EC.visibility_of(ready_button))
+    buttons["save"].click()
+    wait.until(EC.visibility_of(buttons["ready"]))
     return redirect(url_for('index'))
 
 @app.route('/print')
 def print_scores():    
-    print_button.click()
+    buttons["print"].click()
     wait.until(EC.alert_is_present())
     alert = driver.switch_to_alert()
     alert.accept()    
@@ -59,14 +62,34 @@ def print_scores():
 
 @app.route('/receive')
 def receive():
-    receive_button.click()
-    wait.until(EC.visibility_of(ready_button))
+    buttons["receive"].click()
+    try:
+        wait.until(EC.visibility_of(buttons["ready"]))
+    except UnexpectedAlertPresentException:
+        alert = driver.switch_to_alert()
+        alert.accept()
+        try:
+            wait.until(EC.visibility_of(buttons["ready"]))
+        except  StaleElementReferenceException:
+            buttons["ready"] = driver.find_element_by_id("readybutton")
+            wait.until(EC.visibility_of(buttons["ready"]))
+    driver.find_element_by_id("editbutton").click()
+    wait.until(EC.presence_of_element_located((By.ID,"VEST1")))
+    global name
+    name = driver.find_element_by_id("VEST1").get_attribute("value")
+    driver.back()
+    try:
+        wait.until(EC.visibility_of(buttons["ready"]))
+    except  StaleElementReferenceException:
+        buttons["ready"] = driver.find_element_by_id("readybutton")
+        wait.until(EC.visibility_of(buttons["ready"]))
     return redirect(url_for('index'))
 
 @app.route('/abort')
 def abort():
-    abort_button.click()
-    wait.until(EC.visibility_of(save_button))
+    buttons["abort"].click()
+    wait.until(EC.visibility_of(buttons["save"]))
+    wait.until(EC.invisibility_of_element_located((By.ID, 'savebutton')))
     return redirect(url_for('index'))
     
 if __name__ == '__main__':
